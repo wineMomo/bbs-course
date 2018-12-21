37cefa227ce5ef19e36f05ca41e6a8f9ed4a5297
unknown <414561517@qq.com>
2018-12-21 18:35:57 +0800

    '初始化'

---
+++ src/main/java/com/zufe/bbscourse/BbsCourseApplication.java
@@ -0,0 +1,14 @@
+package com.zufe.bbscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbsCourseApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/zufe/bbscourse/BbsCourseApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zufe.bbscourse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BbsCourseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
